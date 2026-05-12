a96481345ff052c2a902e93b3d2f184bcc5a36c6
AJ <agrimj.81@gmail.com>
2026-05-12 14:58:00 +0530

    Implement task completion feature

--- web.py
+++ web.py
@@ -13,8 +13,14 @@ st.subheader("Use this app to increase your productivity!")
 
 st.text("Start the day with accountability and discipline!")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
 
 st.text_input(label="ADD TASK",placeholder="Add a new task",
               on_change=add_todo, label_visibility="hidden", key="new_task")
