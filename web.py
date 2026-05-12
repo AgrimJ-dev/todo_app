265a9eaf3cb1d6b2b9b457332f0c4b14287bb100
AJ <agrimj.81@gmail.com>
2026-05-12 14:41:25 +0530

    Implement "add new tasks" featur

--- web.py
+++ web.py
@@ -3,6 +3,11 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    new_task = st.session_state["new_task"] + "\n"
+    todos.append(new_task)
+    functions.write_todos(todos)
+
 st.title("My Todo App")
 st.subheader("Use this app to increase your productivity!")
 
@@ -11,4 +16,6 @@ st.text("Start the day with accountability and discipline!")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="",placeholder="Add a new task")
+st.text_input(label="ADD TASK",placeholder="Add a new task",
+              on_change=add_todo, label_visibility="hidden", key="new_task")
+
